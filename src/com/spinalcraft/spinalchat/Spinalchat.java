beaba41d62d017107956b67be53b455a6a80c03f
Parker Kemp <parker.kemp@hitcents.com>
2014-07-15 13:27:34 -0500

    Add dependencies to Dynmap and Essentials. Deny web chat from Essentials muted players.

--- src/com/spinalcraft/spinalchat/Spinalchat.java
+++ src/com/spinalcraft/spinalchat/Spinalchat.java
@@ -1,5 +1,43 @@
 package com.spinalcraft.spinalchat;
 
-public class Spinalchat {
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.DynmapWebChatEvent;
 
+
+import com.earth2me.essentials.Essentials;
+import com.spinalcraft.spinalpack.Co;
+import com.spinalcraft.spinalpack.Spinalpack;
+
+public class Spinalchat extends JavaPlugin implements Listener{
+	private Essentials ess;
+	static ConsoleCommandSender console;
+	
+	@Override
+	public void onEnable(){
+		console = Bukkit.getConsoleSender();
+		
+		console.sendMessage(Spinalpack.code(Co.BLUE) + "Spinalchat online!");
+		getServer().getPluginManager().registerEvents((Listener)this,  this);
+
+		ess = (Essentials)Bukkit.getServer().getPluginManager().getPlugin("Essentials");
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled=true)
+	public void onDynmapWebChat(DynmapWebChatEvent event){
+		if(ess.getUser(event.getName()).isMuted()){
+			console.sendMessage(Spinalpack.code(Co.RED) + "Spinalchat denied web message from muted player \"" + event.getName() + "\".");
+			event.setCancelled(true);
+		}
+			
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
 }
